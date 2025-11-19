6539a06eb7a58ada5ebfa8ca708e7bb6184283af
ceaniracle <nddindaaysrhaw@gmail.com>
2025-11-19 07:22:24 +0700

    tess

---
+++ bin/expense_micel.dart
@@ -0,0 +1,5 @@
+import 'package:expense_micel/expense_micel.dart' as expense_micel;
+
+void main(List<String> arguments) {
+  print('Hello world: ${expense_micel.calculate()}!');
+}
---
+++ test/expense_micel_test.dart
@@ -0,0 +1,8 @@
+import 'package:expense_micel/expense_micel.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
